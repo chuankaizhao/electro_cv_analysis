9bf693742ed072fb2fffc39bcdf6bd83cb37287a
chuankaizhao <ckzhao813@gmail.com>
2021-04-19 23:25:14 -0500

    expand excel data columns

--- parse_input.py
+++ parse_input.py
@@ -1,15 +1,29 @@
 import pandas as pd
 import numpy  as np
+import re
 
 MOVING_WINDOW_SIZE = 3
 
-def parse_data(df, data):
-    row, col = int(data[1:]), ord(data[0].upper()) - ord('A')
+regex = re.compile(r'(\d+|\s+)')
+
+def parse_key(location):
+    location = regex.split(location)
+    row, col = location[1], location[0]
+    if len(col) == 1:
+        col = ord(col[0].upper()) - ord('A')
+    else:
+        col = (ord(col[0].upper()) - ord('A') + 1) * 26 + ord(col[1].upper()) - ord('A')    
+    return int(row), col
+
+def parse_data(df, location):
+    row, col = parse_key(location)
+    print(row, col)
     data = df.loc[row:, [col]].to_numpy().transpose()[0]
     return row, col, data
 
-def parse_smooth_data(df, data):
-    row, col = int(data[1:]), ord(data[0].upper()) - ord('A')
+def parse_smooth_data(df, location):
+    row, col = parse_key(location)
+    print(row, col)
     data = df.loc[row:, [col]].rolling(MOVING_WINDOW_SIZE, min_periods=1, 
                                                               center=True).mean().to_numpy().transpose()[0]
     return row, col, data
